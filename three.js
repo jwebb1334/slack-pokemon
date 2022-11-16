c59030cdd8b687a7b46098f651e1a8ab4395a040
webbjack98 <webb@gosolo.io>
2022-11-16 16:36:33 -0700

    update

---
+++ three.js
@@ -0,0 +1,10 @@
+const { Scene, PerspectiveCamera, WebGLRenderer } = require('three');
+
+const scene = new Scene();
+
+// const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// const renderer = new WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+console.log(scene)
